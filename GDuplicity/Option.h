ce1588566f4dba8cb2a900640f7cd0a67ac4dcd3
Hubert <mathsmac@yahoo.fr>
2013-08-21 19:15:25 +0200

    * ras

---
+++ GDuplicity/Option.h
@@ -0,0 +1,18 @@
+//
+//  Option.h
+//  GDuplicity
+//
+//  Created by Hubert on 21/08/13.
+//  Copyright (c) 2013 Hubert. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Option : NSObject {
+    BOOL allow_source_mismatch;
+}
+
+
+- (void) test;
+
+@end
---
+++ GDuplicity/Option.m
@@ -0,0 +1,17 @@
+//
+//  Option.m
+//  GDuplicity
+//
+//  Created by Hubert on 21/08/13.
+//  Copyright (c) 2013 Hubert. All rights reserved.
+//
+
+#import "Option.h"
+
+@implementation Option
+
+-(void) test {
+    self->allow_source_mismatch = "e";
+}
+
+@end

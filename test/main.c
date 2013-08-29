92d0d310dd9a9cdf7cd5c7aa2fd49e25ae6d466b
Hubert <mathsmac@yahoo.fr>
2013-08-29 11:58:40 +0200

    * [Global] share presets

---
+++ test/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  test
+//
+//  Created by Hubert on 29/08/13.
+//  Copyright (c) 2013 Codéin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
